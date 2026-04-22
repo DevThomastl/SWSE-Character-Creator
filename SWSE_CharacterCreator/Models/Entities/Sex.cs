e60810aec126a512c10ed55b4ef00f2937ac7f43
Thomas <thomastl.tech@gmail.com>
2026-04-22 16:42:12 -0400

    Delete SWSE_CharacterCreator directory

--- SWSE_CharacterCreator/Models/Entities/Sex.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SWSE_CharacterCreator.Models.Entities;
-
-public enum Sex : byte
-{
-    Unspecified = 0,
-    Male = 1,
-    Female = 2,
-    Other = 3
-}
--- SWSE_CharacterCreator/Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SWSE_CharacterCreator.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
